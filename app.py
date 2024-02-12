017369f27f3148fc0e523fb07427433f66729018
nilanyK <101003950+nilanyK@users.noreply.github.com>
2024-02-12 14:42:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 from pathlib import Path
 
 # Get the directory where this script resides
 script_directory = Path(__file__).parent
 
-num_chunks=9
+num_chunks = 9
+
 # Define a function to read and concatenate all CSV file chunks
 @st.cache
 def read_and_concat_chunks():
@@ -26,7 +28,15 @@ def read_and_concat_chunks():
     return combined_df
 
 # Call the function to read and concatenate chunks
-combined_df = read_and_concat_chunks()
+equipements_df = read_and_concat_chunks()
 
-# Display the head of the combined DataFrame
-st.write(combined_df.head())
+# Filtrer les données selon les conditions spécifiées
+filtered_df = equipements_df[(equipements_df['statut_site'] == 'inactif') & (equipements_df['Libelle statut'] != 'Hors Contrat')]
+
+# Grouper par Code superviseur et compter le nombre d'équipements
+grouped_df = filtered_df.groupby('Code superviseur').size().reset_index(name='Nombre d\'équipements')
+
+# Créer l'histogramme avec Plotly
+fig = px.bar(grouped_df, x='Code superviseur', y='Nombre d\'équipements', title="Nombre d\'équipements de site inactifs dont le statut n'est pas hors contrat")
+# Afficher le graphique dans Streamlit
+st.plotly_chart(fig)
